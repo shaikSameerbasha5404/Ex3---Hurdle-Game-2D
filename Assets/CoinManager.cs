236e0f63a4a980f8f71ed854f98237d7db2452d7
SHAIK SAMEER BASHA <118707756+shaikSameerbasha5404@users.noreply.github.com>
2024-05-09 16:17:18 +0530

    Update CoinManager.cs

--- Assets/CoinManager.cs
+++ Assets/CoinManager.cs
@@ -1,1 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
+public class CoinManager : MonoBehaviour
+{
+    public int coincount;
+    public Text value;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        value.text = coincount.ToString(); 
+    }
+}
